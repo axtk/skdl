7687e235543e3731f067fc0399574cd0c2a0bb0e
axtk <axtkx@inbox.ru>
2024-04-07 08:46:21 +0300

    refresh schedule tests

--- tests.ts
+++ tests.ts
@@ -1,64 +1,95 @@
-import {schedule} from './src/schedule';
+import { schedule } from './src/schedule';
 
-const SEC = 1e3;
-
-let showElapsedTimes = true;
 let f = () => console.log('.');
 
-let k = 0;
-let r = () => {
-    let v = {x: k++ < 3 ? 'pending' : 'completed'};
-    console.log(v);
-    return Promise.resolve(v);
-};
+async function test(
+    title: string,
+    fn: () => unknown | Promise<unknown>,
+    estimatedTime?: number,
+) {
+    console.log(title);
 
-async function test(title: string, fn: () => unknown | Promise<unknown>, time?: number) {
-    console.log('\n' + title);
     let t0 = Date.now();
     await fn();
     let dt = Date.now() - t0;
-    if (showElapsedTimes)
-        console.log(`Elapsed: ${(dt/SEC).toFixed(3)}"`);
-    if (time !== undefined && Math.abs(time*SEC - dt) > 150)
-        throw new Error(`Out of time bounds: '${title}'; expected: ${time}"`);
+
+    console.log(`Elapsed: ${dt} ms`);
+
+    if (estimatedTime !== undefined && Math.abs(estimatedTime - dt) > 100)
+        throw new Error(`Out of time bounds; expected: ${estimatedTime}, actual time: ${dt}`);
+
+    console.log();
 }
 
 (async () => {
 
-await test('-; -', schedule(f), 0);
+await test('schedule, no options', async () => {
+    let run = schedule(f);
+    await run();
+}, 0);
 
-await test('false; .5"', schedule(f, {
-    repeat: false,
-    delay: .5*SEC,
-}), .5);
+await test('schedule, repeat = false, constant delay', async () => {
+    let run = schedule(f, {
+        repeat: false,
+        delay: 200,
+    });
+    await run();
+}, 200);
 
-await test('undefined; .5"', schedule(f, {
-    delay: .5*SEC,
-}), .5);
+await test('schedule, no repeat, constant delay', async () => {
+    let run = schedule(f, {
+        delay: 200,
+    });
+    await run();
+}, 200);
 
-await test('0; .5"', schedule(f, {
-    repeat: 0,
-    delay: .5*SEC,
-}), .5);
+await test('schedule, repeat = 0, constant delay', async () => {
+    let run = schedule(f, {
+        repeat: 0,
+        delay: 200,
+    });
+    await run();
+}, 200);
 
-await test('3; .5"', schedule(f, {
-    repeat: 3,
-    delay: .5*SEC,
-}), 1.5);
+await test('schedule, repeat = 3, constant delay', async () => {
+    let run = schedule(f, {
+        repeat: 3,
+        delay: 200,
+    });
+    await run();
+}, 600);
 
-await test('3; .5*(1+k)"', schedule(f, {
-    repeat: 3,
-    delay: (_, k) => .5*(1 + k)*SEC,
-}), 3);
+await test('schedule, repeat = 3, linear delay', async () => {
+    let run = schedule(f, {
+        repeat: 3,
+        delay: (_data, iteration) => (iteration + 1)*200,
+    });
+    await run();
+}, 1200);
 
-await test('PI; .25*(1+k)"', schedule(f, {
-    repeat: (_, k) => k < Math.PI,
-    delay: (_, k) => .25*(1 + k)*SEC,
-}), 2.5);
+await test('schedule, repeat < PI, linear delay', async () => {
+    let run = schedule(f, {
+        repeat: (_data, iteration) => iteration < Math.PI,
+        delay: (_data, iteration) => (iteration + 1)*200,
+    });
+    await run();
+}, 2000);
 
-await test('!completed; 1"', schedule(r, {
-    repeat: v => v?.x !== 'completed',
-    delay: .25*SEC,
-}), 1);
+await test('schedule, repeat while status !== completed, constant delay', async () => {
+    let k = 0;
+
+    let poll = () => {
+        let status = k++ < 3 ? 'pending' : 'completed';
+        console.log(`status: ${status}`);
+        return Promise.resolve({ status });
+    };
+
+    let run = schedule(poll, {
+        repeat: data => data?.status !== 'completed',
+        delay: 200,
+    });
+
+    await run();
+}, 800);
 
 })();
