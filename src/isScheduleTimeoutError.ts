8464f5c8a59bd5941613bdf01733b3f6f6cb31da
axtk <axtkx@inbox.ru>
2024-04-07 14:41:34 +0300

    add type predicate to timeout error check

--- src/isScheduleTimeoutError.ts
+++ src/isScheduleTimeoutError.ts
@@ -1,5 +1,5 @@
 import { SCHEDULE_TIMEOUT } from './const';
 
-export function isScheduleTimeoutError(error: unknown) {
+export function isScheduleTimeoutError(error: unknown): error is Error {
     return error instanceof Error && error.message === SCHEDULE_TIMEOUT;
 }
